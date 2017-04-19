1b7b729bc851ba11fa1c82cd2de7acf54b6e9248
Natte Sharan <nattesharan@gmail.com>
2017-04-19 20:51:55 +0530

    Add files via upload

---
+++ build/assets.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "client": {
+    "js": "/assets/client.js"
+  },
+  "admin": {
+    "js": "/assets/admin.chunk.js"
+  },
+  "polyfills": {
+    "js": "/assets/polyfills.chunk.js"
+  },
+  "vendor": {
+    "js": "/assets/vendor.js"
+  }
+};
